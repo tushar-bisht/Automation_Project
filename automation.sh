6a8a3a81abbfcc03dbc2a4161e1c8c5e21774320
root <root@ip-172-31-16-207.ec2.internal>
2022-05-18 08:26:50 +0000

    Task 2 and 3

--- automation.sh
+++ automation.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#################################Task 2 ###################################################
 sudo apt-get update -y
 sudo apt-get install apache2 -y
 sudo service apache2 start
@@ -10,3 +11,23 @@ name=tusharbisht
 tar -cvf $name-httpd-logs-$timestamp.tar /var/log/apache2/*.log
 sudo apt install awscli -y
 aws s3 cp /tmp/$name-httpd-logs-$timestamp.tar s3://$s3/$name-httpd-logs-$timestamp.tar
+
+################################Task 3 #####################################################
+cd /var/www/html/
+if [ -f /var/www/html/inventory.html ]; then
+echo "File exist"
+else
+touch inventory.html
+echo -e "Log Type\t\t Date Created\t\t\t Type\t\t Size" >>/var/www/html/inventory.html
+echo -e "\n" >>/var/www/html/inventory.html
+fi
+cd /tmp
+size=$(ls -lah /tmp | grep tusharbisht | grep $timestamp | awk '{print $5}')
+echo -e "httpd-logs\t\t $timestamp\t\t tar\t\t ${size}">>/var/www/html/inventory.html
+
+if [ -f /etc/cron.d/automation ]; then
+echo "File exist"
+else
+touch /etc/cron.d/automation
+echo "@daily root /root/Automation_Project/automation.sh" >/etc/cron.d/automation
+fi
